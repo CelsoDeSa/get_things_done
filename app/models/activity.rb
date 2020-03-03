9183b7bca39bfbd7dcad2cb41221ed74a2fd5277
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2020-03-02 23:01:08 -0300

    Corrected erroneous validation in activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :project
   validates :name, :begin, :end, presence: { message: "não pode estar em branco"}
-  validates :name, uniqueness: { message: "tem que ser única"}
   scope :due, -> { where(finished: false).order(end: :desc) }
   scope :finished, -> { where(finished: true) }
 end
